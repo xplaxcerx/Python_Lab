4d03248eaa8b2c21c1782a67e9a7180472adb4c3
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-02-05 23:52:22 +0500

    Add files via upload

---
+++ Lab1/00_distance.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть словарь координат городов
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+distances = {}
+
+# TODO здесь заполнение словаря
+distances['Moscow - London'] = ((sites['Moscow'][0] - sites['London'][0])**2 + (sites['Moscow'][1] - sites['London'][1])**2)**0.5
+distances['Moscow - Paris'] = ((sites['Moscow'][0] - sites['Paris'][0])**2 + (sites['Moscow'][1] - sites['Paris'][1])**2)**0.5
+distances['Paris - London'] = ((sites['Paris'][0] - sites['London'][0])**2 + (sites['Paris'][1] - sites['London'][1])**2)**0.5
+print(distances)
+
+
+
+
+
