06520d3c1c1fd4bebdfca45bf749aa4f60af6e34
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-05-18 09:49:26 +0500

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,16 +1,13 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, text
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# создаем движок базы данных и сессию
 engine = create_engine('sqlite:///university.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# создаем базовый класс для моделей
-Base = declarative_base()
 
-# модель "Дисциплины"
+Base = declarative_base()
 class Discipline(Base):
     __tablename__ = 'disciplines'
 
@@ -26,7 +23,6 @@ class Discipline(Base):
     def __repr__(self):
         return f"<Discipline(name='{self.name}', lectures='{self.lectures}', practices='{self.practices}', labs='{self.labs}')>"
 
-# модель "Кафедры"
 class Department(Base):
     __tablename__ = 'departments'
 
@@ -37,10 +33,8 @@ class Department(Base):
     def __repr__(self):
         return f"<Department(name='{self.name}')>"
 
-# создаем таблицы в базе данных
 Base.metadata.create_all(engine)
 
-# заполняем базу данных тестовыми данными
 department1 = Department(name='Department of Mathematics')
 department2 = Department(name='Department of Physics')
 
@@ -50,34 +44,13 @@ discipline3 = Discipline(name='Quantum Mechanics', lectures=50, practices=15, la
 
 session.add_all([department1, department2, discipline1, discipline2, discipline3])
 session.commit()
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///university.db', echo=True)
-Base = declarative_base()
+def execute_user_query():
+    user_query = input("Enter your SQL query: ")
+    result = session.execute(text(user_query))
+    for row in result:
+        print(row)
 
-class Discipline(Base):
-    __tablename__ = 'discipline'
+execute_user_query()
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    lectures = Column(Integer)
-    practices = Column(Integer)
-    labs = Column(Integer)
-    department = Column(String)
-
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-discipline1 = Discipline(name='Math', lectures=30, practices=15, labs=20, department='Mathematics')
-discipline2 = Discipline(name='Physics', lectures=20, practices=10, labs=15, department='Physics')
-discipline3 = Discipline(name='Chemistry', lectures=25, practices=20, labs=25, department='Chemistry')
-
-session.add_all([discipline1, discipline2, discipline3])
-session.commit()
-
-
-print('База данных успешно создана и заполнена тестовыми данными.')
+session.close()
